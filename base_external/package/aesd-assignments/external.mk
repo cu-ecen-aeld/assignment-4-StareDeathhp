8abf80cdec249cb1fb97b5842e24a39a27b69ed9
StareDeathhp <saclang1411@gmail.com>
2025-05-18 09:10:47 +0700

    assignment4

--- base_external/package/aesd-assignments/external.mk
+++ base_external/package/aesd-assignments/external.mk
@@ -1,17 +1,1 @@
-AESD_ASSIGNMENTS_VERSION = main
-AESD_ASSIGNMENTS_SITE = git@github.com:yourusername/assignments-3-and-later-<YourGHUsername>.git
-AESD_ASSIGNMENTS_DEPENDENCIES =
-AESD_ASSIGNMENTS_LICENSE = MIT
-AESD_ASSIGNMENTS_TERM = Finder scripts and utilities
-
-define AESD_ASSIGNMENTS_BUILD_CMDS
-    # copy writer, finder.sh, tester.sh, conf/* into $(TARGET_DIR)/usr/bin
-    mkdir -p $(TARGET_DIR)/usr/bin
-    cp $$(BUILD_DIR)/aesd-assignments/finder-app/writer $(TARGET_DIR)/usr/bin/
-    cp $$(BUILD_DIR)/aesd-assignments/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-    cp $$(BUILD_DIR)/aesd-assignments/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-    mkdir -p $(TARGET_DIR)/etc/finder-app/conf
-    cp $$(BUILD_DIR)/aesd-assignments/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-endef
-
-AESD_ASSIGNMENTS_BUILD_CMDS +=
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
